b2f1f4abd5fb353eabfcdada05c6f19bc9da4ac9
ruchitaghodasara <ruchighodasara16@gmail.com>
2022-05-05 11:02:07 +0530

    1 file modified

---
+++ index.js
@@ -0,0 +1,34 @@
+function fullNameno() {
+  let fName = 'Ruchita',
+    lName = 'Rajpara';
+  return fName + ' ' + lName;
+}
+
+function fullName(fName, lName) {
+  return fName + ' ' + lName;
+}
+
+console.log(fullName('Ruchita', 'Ghodasara'));
+console.log(fullNameno());
+
+var addNo = (...args) => [...args].reduce((a, b) => a + b, 0);
+var addNor = (a, b) => [a, b].reduce((a, b) => a + b, 0);
+
+console.log(addNo(2, 3));
+console.log(addNor(2, 3));
+
+function areaOfRectangle(length, width) {
+  let area = length * width;
+
+  return area;
+}
+
+console.log(areaOfRectangle(2, 3));
+
+function convertCelsiusToFahrenheit(cel) {
+  let fernhit = cel * 1.8 + 32;
+
+  return fernhit;
+}
+
+console.log(convertCelsiusToFahrenheit(20));
